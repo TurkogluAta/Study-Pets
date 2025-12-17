1506c87803f49a88a7751235cfb787f178df25d7
TurkogluAta <x21247129@student.ncirl.ie>
2025-12-17 19:32:39 +0000

    cors update to allow all

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,17 +7,12 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    # Allow localhost for development and Render frontend in production
-    origins(
-      "http://localhost:3000",
-      "http://localhost:8000",
-      /\Ahttps:\/\/.*\.onrender\.com\z/,
-      "https://study-pet.onrender.com"
-    )
+    # Allow all origins
+    origins "*"
 
     resource "*",
       headers: :any,
       methods: [ :get, :post, :put, :patch, :delete, :options, :head ],
-      credentials: true
+      credentials: false
   end
 end
